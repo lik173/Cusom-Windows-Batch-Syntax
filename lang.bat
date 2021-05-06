ad8b92b7f36874c8c60f4ba867697096c3c69da1
lik173 <70771696+lik173@users.noreply.github.com>
2021-05-06 16:19:14 +0300

    Add files via upload

--- lang.bat
+++ lang.bat
@@ -14,6 +14,6 @@ goto help
 :compile
 powershell Set-ExecutionPolicy Bypass -Scope CurrentUser
 echo $a = Get-Content ".\%filein%" > compile.ps1
-echo $a = $a -replace '@say ', 'echo ' -replace '@halt', 'exit' -replace '@## ', 'rem ' -replace '@off', '@echo off' -replace '@head ', 'title ' -replace '@input ', 'set /p ' -replace '@stop', 'pause' -replace '@wait ', 'timeout /nobreak /t ' -replace '@_', ':' -replace '@math ', 'set /a ' -replace '@ps ', 'powershell ' -replace '@kill ', 'taskkill /e /im ' -replace '@run', 'start' -replace '#this file is a target', '' -replace '@get-date', 'echo %%date%%' -replace '@get-time', 'echo %%time%%' -replace '@arch', '%%PROCESSOR_ARCHITECTURE%%' -replace '@userprofile', '%%USERPROFILE%%' -replace '@username', '%%USERNAME%%' >> compile.ps1
+echo $a = $a -replace '@say ', 'echo ' -replace '@halt', 'exit' -replace '@## ', 'rem ' -replace '@off', '@echo off' -replace '@head ', 'title ' -replace '@input ', 'set /p ' -replace '@stop', 'pause' -replace '@wait ', 'timeout /nobreak /t ' -replace '@_', ':' -replace '@math ', 'set /a ' -replace '@ps ', 'powershell ' -replace '@kill ', 'taskkill /e /im ' -replace '@run', 'start' -replace '#this file is a target', '' -replace '@get-date', 'echo %%date%%' -replace '@get-time', 'echo %%time%%' -replace '@arch', '%%PROCESSOR_ARCHITECTURE%%' -replace '@userprofile', '%%USERPROFILE%%' -replace '@username', '%%USERNAME%%' -replace '@jmp ', 'goto ' >> compile.ps1
 echo echo $a ^| Out-File .\%out% -Encoding ASCII >> compile.ps1
 powershell .\compile.ps1
